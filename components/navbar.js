42149b0f95605f1934bcce8efbbafb70fb21727e
sumitraghavwork <sumitraghavwork@gmail.com>
2022-07-20 09:38:19 +0530

    home navbar footer

---
+++ components/navbar.js
@@ -0,0 +1,89 @@
+export { navbar }
+
+let navbar = () => {
+  return `<div>
+    <img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6078144624947483c31b964f_logo-white.svg" alt="logo">
+</div>
+<div>
+    <div class="fsec">
+        <div class="dropdown_btn">Product
+            <div class="dropdown_menu fsec">
+                <div class="item">Identity
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8085a3c456b442357_icon-pipeline.svg" alt=""> PipeLine</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d86296f7d9ed0d4b8d_icon-person-add.svg" alt=""> Hiring Team</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d7e765e5e49a6290ae_icon-career-hub.svg" alt=""> Career Hub</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d79f5ef3126c924396_icon-code.svg" alt=""> Api & Workspace</div>                         
+                </div>
+                <div class="item">Source
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d75a563909b8f15249_icon-multi-posting.svg" alt="">Multi-Posting</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8f158802ce086434f_icon-search.svg" alt=""> Sourcing Tools</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8c0a89d87da2ecf34_icon-share.svg" alt=""> ReferralsHub</div>                        
+                </div>
+                <div class="item">Interview
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8085a3c456b442357_icon-pipeline.svg" alt=""> PipeLine</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d86296f7d9ed0d4b8d_icon-person-add.svg" alt=""> Hiring Team</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d7e765e5e49a6290ae_icon-career-hub.svg" alt=""> Career Hub</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d79f5ef3126c924396_icon-code.svg" alt=""> Api & Workspace</div>                         
+                
+                </div>
+                <div class="item">Hire
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d75a563909b8f15249_icon-multi-posting.svg" alt="">Multi-Posting</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8f158802ce086434f_icon-search.svg" alt=""> Sourcing Tools</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8c0a89d87da2ecf34_icon-share.svg" alt=""> ReferralsHub</div>                        
+                
+                </div>
+                <div class="item">Improve
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8085a3c456b442357_icon-pipeline.svg" alt=""> PipeLine</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d86296f7d9ed0d4b8d_icon-person-add.svg" alt=""> Hiring Team</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d7e765e5e49a6290ae_icon-career-hub.svg" alt=""> Career Hub</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d79f5ef3126c924396_icon-code.svg" alt=""> Api & Workspace</div>                         
+                
+                </div>
+            </div>
+        </div>
+        <div><a href="">Collaborative Hiring</a></div>
+        <div><a href="">Pricing</a></div>
+        <div class="dropdown_btn">Resources
+            <div class="dropdown_menu fsec">
+                <div class="item">Learn
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60c0ad1a6e19ad950d31d2bc_icon-blog.svg" alt=""> Blog</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d6910c924b71f18b1e64_Downloadables.svg" alt=""> Downloadables</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d691bf9f573ae05fed00_Events%20and%20%20Webinars.svg" alt=""> Webinars</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d69150acbb9e41642ba4_Podcast.svg" alt=""> Podcast</div> 
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d7c863522b00d31f3c_icon-collaborative.svg" alt="">Collaborative</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60c0ad27be2956d68c49e0bc_icon-help-center.svg" alt=""> Help Center</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d691efcd353adfa9dbdd_Roadmap.svg" alt=""> Product RoadMap</div>                           
+                </div>
+                <div class="item">Featured
+                    <hr>
+                    <div><img class="piggy_bank" src="https://assets-global.website-files.com/60870cca519aca7849a9262d/62c69c760cbc5f15fa4c9342_pay%20transparency.jpg" alt=""></div>                     
+                    <div>JULY 8, 2022</div>
+                    <div>4 great tips for understanding pay transparency</div>
+                </div>
+                <div class="item grey">Blog Categories
+                    <hr>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60c0ad1a6e19ad950d31d2bc_icon-blog.svg" alt=""> Blog</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d6910c924b71f18b1e64_Downloadables.svg" alt=""> Downloadables</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d691bf9f573ae05fed00_Events%20and%20%20Webinars.svg" alt=""> Webinars</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/6278d69150acbb9e41642ba4_Podcast.svg" alt=""> Podcast</div> 
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d8085a3c456b442357_icon-pipeline.svg" alt=""> PipeLine</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d86296f7d9ed0d4b8d_icon-person-add.svg" alt=""> Hiring Team</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d7e765e5e49a6290ae_icon-career-hub.svg" alt=""> Career Hub</div>
+                    <div><img src="https://assets-global.website-files.com/60780a9b4720a47267a88257/60ae39d79f5ef3126c924396_icon-code.svg" alt=""> Api & Workspace</div>                         
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fsec">
+        <div><a href="">Login</a></div>
+        <div><a href="">Get Demo</a></div>
+        <div>Give it a shot</div>
+    </div>
+</div>`
+}
