881e7615bb8f66c41d63e0269df478b84f89cc74
sumitraghavwork <sumitraghavwork@gmail.com>
2022-10-12 08:45:20 +0530

    signup update

---
+++ components/condition.js
@@ -0,0 +1,17 @@
+function condition(){
+    return `
+    <div>
+    <img src="svg/arrow_right_alt_FILL0_wght400_GRAD0_opsz48.png" alt="" id="right1">
+    <p id="para1">Between 8 and 72 characters</p>
+</div>
+<div>
+    <img src="svg/arrow_right_alt_FILL0_wght400_GRAD0_opsz48.png" alt=""  alt="" id="right2">
+    <p id="para2">Upper and lower case letters</p>
+</div>
+<div>
+    <img src="svg/arrow_right_alt_FILL0_wght400_GRAD0_opsz48.png" alt=""  alt="" id="right3">
+    <p id="para1">At least one number</p>
+</div>
+        `
+}
+export default condition;
--- signup page/signup.js
+++ signup page/signup.js
@@ -1,52 +1,45 @@
-
-  
-let ip=(id)=>{
-   return document.getElementById(id).value
+let ip = (id) => {
+  return document.getElementById(id).value
 }
 
-let userip=async()=>{ 
-    let Cname=ip("admin_company_name");
-    let email=ip("user_email");
-    let pass=ip("user_pass")
-    let username=ip("user_name");
-    let phone=ip("user_no");
-    let des=ip("first_name")
+let userip = async () => {
+  let Cname = ip('cname')
+  let email = ip('ename')
+  let pass = ip('mypassword')
+  let username = ip('ename')
+  let phone = ip('user_no')
+  let des = ip('First_name')
 
-    let userdata=new signup(Cname,email,pass,username,phone,des)
-    console.log(userdata)
-    
-    let serverlink=`https://masai-api-mocker.herokuapp.com/auth/register`
-   
-    let res=await fetch(serverlink,{
-      method:"POST",
-      body:JSON.stringify(userdata),
-      headers:{
-        "Content-Type":"application/json"
-      },
+  let userdata = new signup(Cname, email, pass, username, phone, des)
+  console.log(userdata)
 
-    });
- let data=await res.json()
- if(data.error==false){
-    alert("Register Successful")
- 
-    window.location.href="../login page/login.html"
- }
- else{
-    alert("chek credential")
- }
+  let serverlink = `https://masai-api-mocker.herokuapp.com/auth/register`
 
+  let res = await fetch(serverlink, {
+    method: 'POST',
+    body: JSON.stringify(userdata),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  let data = await res.json()
+  if (data.error == false) {
+    alert('Registeration Successful')
 
+    window.location.href = '../login page/login.html'
+  } else {
+    alert('check credentials')
+  }
 }
 
-class signup{
-    constructor(n,e,p,u,m,d){
-       this.name=n;
-       this.email=e;
-       this.password=p;
-       this.username=u;
-       this.mobile=m;
-      
-       
-       this.description=d
-    }
+class signup {
+  constructor(n, e, p, u, m, d) {
+    this.name = n
+    this.email = e
+    this.password = p
+    this.username = u
+    this.mobile = m
+
+    this.description = d
+  }
 }
